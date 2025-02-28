c741a997f612e375ae08621b7c861ed7d3af3824
SIVAPRASAD <sivaprasad.g@live.com>
2025-02-28 18:46:41 +0530

    initial_commit

---
+++ variable.js
@@ -0,0 +1,27 @@
+// Declare a new variable named petDog and give it the name Rex.
+// Declare a new variable named petCat and give it the name Pepper.
+// Log the petDog variable to the console.
+// Log the  petCat variable to the console.
+// Log the following to the console: the text "My pet dog's name is: " and the petDog variable.
+// Log the following to the console: the text "My pet cat's name is: " and the petCat variable.
+// Declare another variable and name it catSound. Assign the string of "purr" to it.
+// Declare another variable and name it dogSound. Assign the string of "woof" to it.
+// Log the following to the console: the variable petDog, then the string "says", then the variable dogSound.
+// Log the following to the console: the variable petCat, then the string "says", then the variable catSound.
+// Reassign the value stored in catSound to the string "meow".
+// Log the following to the console: the variable petCat, then the string "now says", then the variable catSound.
+
+let petDog = "Rex";
+let petCat = "Pepper";
+
+console.log(petDog);
+console.log(petCat);
+console.log("My pet dog's name is: " + petDog);
+console.log("My pet cat's name is: " + petCat);
+let catSound = "purr";
+let dogSound = "Woof";
+console.log(petDog, "says", dogSound);
+console.log(petCat, "says", catSound);
+catSound = "meow";
+console.log(petCat, "now says", catSound);
+console.log();
